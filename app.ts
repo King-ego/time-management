7f66f3d4fe1e94457d0ad651a41bdfbe3bd267ca
diego <diegosantos4957@gmail.com>
2024-11-19 17:34:42 -0300

    feat: await database initialization

--- app.ts
+++ app.ts
@@ -1,15 +1,44 @@
-import express from "express";
+import "reflect-metadata";
+import express, {Request, Response, NextFunction} from "express";
 import cors from "cors";
 import routes from "./src/shared/infra/routes";
+import primaClient from "./src/shared/infra/primaClient";
+import {errors} from "celebrate";
+import AppError from "./src/shared/error";
 
 const app = express();
 
-app.use(express.json());
+primaClient.$connect().then(()=>{
+    app.use(express.json());
 
-app.use(cors());
+    app.use(cors());
 
-app.use(routes);
+    app.use(routes);
 
-app.listen(3100, () => {
-    console.log("Server is running on port 3100");
+    app.use(errors());
+
+    app.use((err: Error, __: Request, response: Response, _:NextFunction) => {
+        if (err instanceof AppError) {
+            response.status(err.statusCode).json({
+                status: "error",
+                message: err.message
+            });
+        }
+
+        console.error(err);
+
+        response.status(500).json({
+            status: "error",
+            message: "Internal server error"
+        });
+    });
+
+
+    app.listen(3100, () => {
+        console.log("Server is running on port 3100");
+    });
+}).catch((err)=>{
+    console.error("Failed to connect to the database:", err);
+    process.exit(1);
 });
+
